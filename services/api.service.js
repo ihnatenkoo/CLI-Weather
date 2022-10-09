91d2e8523ec214ae7706235423fcc92a6723b431
Dmytro <ihnatenko.dev@gmail.com>
2022-10-09 12:31:37 +0300

    Upd api.service.js using axios

--- services/api.service.js
+++ services/api.service.js
@@ -1,4 +1,4 @@
-import https from 'https';
+import axios from 'axios';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 
 const getWeather = async (city) => {
@@ -9,22 +9,18 @@ const getWeather = async (city) => {
 		);
 	}
 
-	const url = new URL(`https://api.openweathermap.org/data/2.5/weather`);
-	url.searchParams.append('q', city);
-	url.searchParams.append('appid', token);
-	url.searchParams.append('units', 'metric');
+	const { data } = await axios.get(
+		`https://api.openweathermap.org/data/2.5/weather`,
+		{
+			params: {
+				q: city,
+				appid: token,
+				units: 'metric',
+			},
+		}
+	);
 
-	https.get(url, (response) => {
-		let res = '';
-
-		response.on('data', (chunk) => {
-			res += chunk;
-		});
-
-		response.on('end', () => {
-			console.log(res);
-		});
-	});
+	console.log(data);
 };
 
 export { getWeather };
