05d1ba685dc09cf9591785d1d2118f986ee1a58d
Dmytro <ihnatenko.dev@gmail.com>
2022-10-09 10:00:49 +0300

    Create log.service.js

---
+++ services/log.service.js
@@ -0,0 +1,20 @@
+import chalk from 'chalk';
+import dedent from 'dedent-js';
+
+const printError = (error) => {
+	console.log(chalk.bgRed.bold(' ERROR ') + '' + error);
+};
+
+const printSuccess = (message) => {
+	console.log(chalk.bgGreen(' SUCCESS ') + '' + message);
+};
+
+const printHelp = () => {
+	console.log(dedent`${chalk.white.bgYellow(' HELP ')}
+							Without parameters - show weather
+							-h show help
+							-s [CITY] set city
+							-t [TOKEN] set token`);
+};
+
+export { printError, printSuccess, printHelp };
